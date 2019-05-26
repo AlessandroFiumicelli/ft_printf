ad2340ba67256c092b4ba9093a33b05071fe9b1c
Rivers <umberto.carletti@gmail.com>
2019-05-26 16:28:39 +0200

    some fix

--- includes/ft_printf_internal.h
+++ includes/ft_printf_internal.h
@@ -12,6 +12,7 @@
 
 #ifndef FT_PRINTF_INTERNAL_H
 # define FT_PRINTF_INTERNAL_H
+# include "ft_printf.h"
 # include <stdarg.h>
 # include <stdint.h>
 
---
+++ test.c
@@ -0,0 +1,8 @@
+#include "includes/ft_printf.h"
+#include <stdio.h>
+
+int	main()
+{
+	printf("%8.2f\n", 3.14);
+	return (0);
+}
